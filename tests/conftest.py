b1ad8b010330e6c75d062befd9738d82604eff7c
swetha-jb <swethajb55@gmail.com>
2025-09-09 14:57:43 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,17 @@ from selenium import webdriver
 
 @pytest.fixture()
 def setup(request):
+
+    chrome_options = Options()
+    user_data_dir = tempfile.mkdtemp()
+    chrome_options.add_argument(f"--user-data-dir={user_data_dir}")  
+
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--headless")  # Optional: run without GUI
+
     driver = webdriver.Chrome()
     driver.maximize_window()
     driver.implicitly_wait(60)
@@ -14,6 +25,7 @@ def setup(request):
     request.cls.driver = driver
     yield
     driver.quit()
+
 
 
 
