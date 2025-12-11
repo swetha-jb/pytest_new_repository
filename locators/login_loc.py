2055c5b4298536bb9fba56c99578a3d6df3c2a27
root <root@vvdn-sfit>
2025-12-11 11:49:20 +0530

    Pushed Successfully

--- locators/login_loc.py
+++ locators/login_loc.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
 
-email_input_loc = (By.XPATH, "//input[@formcontrolname='email']")
+email_input_loc = (By.XPATH, "//input[@forrcontrolname='email']")
 password_input_loc = (By.XPATH, "//input[@formcontrolname='password']")
 signIn_button_loc = (By.ID, "Login-btn")
